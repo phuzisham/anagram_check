86ee1d561eedbe811ddb5ec49bc88001bed0e0a3
David Hale <david.hale7@gmail.com>
2017-09-08 13:54:13 -0700

    refactor instance variable and argument

--- lib/anagram_check_script.rb
+++ lib/anagram_check_script.rb
@@ -6,4 +6,4 @@ str1 = gets.chomp
 puts 'Type second word:'
 str2 = gets.chomp
 
-puts Anagram.new(str1, str2).anagram_check()
+puts Anagram.new(str1).anagram_check(str2)
--- spec/anagram_check_spec.rb
+++ spec/anagram_check_spec.rb
@@ -5,43 +5,43 @@ require('anagram_check')
 describe('#anagram_check') do
 
   it('will check if two words are words') do
-    example = Anagram.new('rby', 'bury')
-    expect(example.anagram_check()).to(eq('You need to input actual words!'))
+    example = Anagram.new('rby')
+    expect(example.anagram_check('bury')).to(eq('You need to input actual words!'))
   end
 
   it('will check if two words are anagrams') do
-    example = Anagram.new('ruby', 'bury')
-    expect(example.anagram_check()).to(eq('These words are anagrams!'))
+    example = Anagram.new('ruby')
+    expect(example.anagram_check('bury')).to(eq('These words are anagrams!'))
   end
 
   it('will check if two words are not anagrams') do
-    example = Anagram.new('mango', 'melon')
-    expect(example.anagram_check()).to(eq('These words are not anagrams!'))
+    example = Anagram.new('mango')
+    expect(example.anagram_check('melon')).to(eq('These words are not anagrams!'))
   end
 
   it('will check ignoring case') do
-    example = Anagram.new('ruBy', 'buRy')
-    expect(example.anagram_check()).to(eq('These words are anagrams!'))
+    example = Anagram.new('ruBy')
+    expect(example.anagram_check('buRy')).to(eq('These words are anagrams!'))
   end
 
   it('will check if words are also palindromes') do
-    example = Anagram.new('Racecar', 'racecar')
-    expect(example.anagram_check()).to(eq('These words are anagrams and palindromes!'))
+    example = Anagram.new('Racecar')
+    expect(example.anagram_check('racecar')).to(eq('These words are anagrams and palindromes!'))
   end
 
   it('will check if words are antigrams') do
-    example = Anagram.new('Kite', 'Car')
-    expect(example.anagram_check()).to(eq('These words are antigrams!'))
+    example = Anagram.new('Kite')
+    expect(example.anagram_check('Car')).to(eq('These words are antigrams!'))
   end
 
   it('will remove spaces and check') do
-    example = Anagram.new('Race Car', 'racecar')
-    expect(example.anagram_check()).to(eq('These words are anagrams and palindromes!'))
+    example = Anagram.new('Race Car')
+    expect(example.anagram_check('racecar')).to(eq('These words are anagrams and palindromes!'))
   end
 
   it('will remove special characters and check') do
-    example = Anagram.new('Race Car', 'racecar!!!##@@')
-    expect(example.anagram_check()).to(eq('These words are anagrams and palindromes!'))
+    example = Anagram.new('Race Car')
+    expect(example.anagram_check('racecar!!!##@@')).to(eq('These words are anagrams and palindromes!'))
   end
 
 end
