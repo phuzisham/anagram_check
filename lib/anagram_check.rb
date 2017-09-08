160f91c94c00a31efe7f4e4eb62b95d7c1168a34
David Hale <david.hale7@gmail.com>
2017-09-08 08:26:38 -0700

    ignore case of letters

--- lib/anagram_check.rb
+++ lib/anagram_check.rb
@@ -1,7 +1,7 @@
 class Anagram
   def anagram_check(str1, str2)
-    str1 = str1.split('').sort
-    str2 = str2.split('').sort
+    str1 = str1.downcase.split('').sort
+    str2 = str2.downcase.split('').sort
 
     if (str1 === str2)
       true
--- spec/anagram_check_spec.rb
+++ spec/anagram_check_spec.rb
@@ -5,8 +5,12 @@ require('anagram_check')
 describe('#anagram_check') do
   example = Anagram.new()
 
-  it() do
+  it('will check if two words are anagrams') do
     expect(example.anagram_check('ruby', 'bury')).to(eq(true))
+  end
+
+  it('will check ignoring case') do
+    expect(example.anagram_check('ruBy', 'burY')).to(eq(true))
   end
 
 end
