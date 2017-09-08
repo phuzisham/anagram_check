499f0ead3a4687f8820fd9926a17b03486d3a260
David Hale <david.hale7@gmail.com>
2017-09-08 15:01:01 -0700

    add original strings to script

--- lib/anagram_check_script.rb
+++ lib/anagram_check_script.rb
@@ -6,4 +6,4 @@ str1 = gets.chomp
 puts 'Type second word:'
 str2 = gets.chomp
 
-puts Anagram.new(str1).anagram_check(str2)
+puts Anagram.new(str1).anagram_check(str2) + ' (' + str1 + ' and ' + str2 + ')'
