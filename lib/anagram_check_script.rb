ff0ba670e955cb03c306cb6958864829603462a3
David Hale <david.hale7@gmail.com>
2017-09-08 15:47:34 -0700

    script offers option to check another string against original string

--- lib/anagram_check_script.rb
+++ lib/anagram_check_script.rb
@@ -6,4 +6,19 @@ str1 = gets.chomp
 puts 'Type second word:'
 str2 = gets.chomp
 
-puts Anagram.new(str1).anagram_check(str2) + ' (' + str1 + ' and ' + str2 + ')'
+example = Anagram.new(str1)
+
+puts example.anagram_check(str2) + ' (' + str1 + ' and ' + str2 + ')'
+
+puts 'Would you like to check another word against ' + str1 + '? Type yes or no'
+
+response = gets.chomp
+response = response.gsub(/[^a-z]/i, '').downcase
+
+if (response === 'yes')
+  puts 'Type another word:'
+  str2 = gets.chomp
+  puts example.anagram_check(str2) + ' (' + str1 + ' and ' + str2 + ')'
+else
+  puts 'Thanks for using this script!'
+end
