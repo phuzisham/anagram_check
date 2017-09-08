5d33f6013d67a6ee676ddfa5f22e455e8eb8425b
Guest User <Guest@Epicodus-3D.local>
2017-09-08 12:58:54 -0700

    make script work with instance class

--- lib/anagram_check_script.rb
+++ lib/anagram_check_script.rb
@@ -1,5 +1,4 @@
 require_relative('anagram_check')
-example = Anagram.new()
 
 puts 'Type first word:'
 str1 = gets.chomp
@@ -7,4 +6,4 @@ str1 = gets.chomp
 puts 'Type second word:'
 str2 = gets.chomp
 
-puts example.anagram_check(str1, str2)
+puts Anagram.new(str1, str2).anagram_check()
